1172d04ad85f0131d6e5518ee8f3bf4eb2f2d6f7
Aaron Bockelie <aaronsb@gmail.com>
2025-02-04 21:16:01 -0600

    Fix Jest configuration for ESM support

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,5 @@ export default {
       },
     ],
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+// Make Jest available globally for ESM
+globalThis.jest = jest;
+globalThis.expect = expect;
+globalThis.describe = describe;
+globalThis.it = it;
+globalThis.beforeEach = beforeEach;
+globalThis.afterEach = afterEach;
+globalThis.beforeAll = beforeAll;
+globalThis.afterAll = afterAll;
