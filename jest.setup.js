979d0395ef3775a43bbce1a7cab951b4313b16d4
Aaron Bockelie <aaronsb@gmail.com>
2025-02-04 21:16:43 -0600

    Update Jest configuration to use CommonJS for test environment

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,2 @@
-// Make Jest available globally for ESM
-globalThis.jest = jest;
-globalThis.expect = expect;
-globalThis.describe = describe;
-globalThis.it = it;
-globalThis.beforeEach = beforeEach;
-globalThis.afterEach = afterEach;
-globalThis.beforeAll = beforeAll;
-globalThis.afterAll = afterAll;
+// Jest setup file
+require('@jest/globals');
