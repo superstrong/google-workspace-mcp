ee36ec36ec110a422494f489cb728ce71306794a
Aaron Bockelie <aaronsb@gmail.com>
2025-02-04 22:04:20 -0600

    fix: configure jest to handle both ts and js files properly

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,12 +7,21 @@ module.exports = {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
+    // Transform TypeScript files
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         useESM: true,
       },
     ],
+    // Transform JavaScript files in build directory
+    '^.+\\.jsx?$': [
+      'babel-jest',
+      {
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+      },
+    ],
   },
+  transformIgnorePatterns: ['/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
 };
